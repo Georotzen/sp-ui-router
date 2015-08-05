049c3c2cba0626cf6d5d30ec67a0756e6cd24a49
Thomas Greco <Thomasjosephgreco@yahoo.com>
2015-08-05 14:23:39 -0400

    Initial commit for sitepoint tutorial

---
+++ app.js
@@ -0,0 +1,24 @@
+angular.module('app', ['ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('home', {
+                url: '/',
+                views: {
+                    '': {
+                        templateUrl: './templates/main.html'
+                    },
+                    'nav': {
+                        templateUrl: './templates/assets/nav.html'
+                    },
+                    'body@home': {
+                        templateUrl: './templates/body.html'
+                    },
+                    'footer@home': {
+                        templateUrl: './templates/assets/footer.html'
+                    }
+                }
+            });
+        //END ROUTE CONFIGURTATION
+    }]);
